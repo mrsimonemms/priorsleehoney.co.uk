318aa1ac0ab97db65d0ff60c943ffcbc6a80b473
Simon Emms <simon@simonemms.com>
2024-08-17 16:12:35 +0000

    feat: create initial astro app

---
+++ src/consts.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2024 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Priorslee Honey';
+export const SITE_DESCRIPTION = 'Small-scale honey producers in Shropshire';
